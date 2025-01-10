43827c7634c1f1c55e5c2e8990cbba8d3b178fb2
abouthimmel <mpm111005@gmail.com>
2025-01-10 21:37:29 +0700

    menambahkan keterangan menggunakna npm, seperti chalk

--- npm-learn/main.js
+++ npm-learn/main.js
@@ -1,1 +1,15 @@
+// di saat meng init sebuah npm, akan muncul package json
+
+const { default: chalk } = require("chalk");
+
 console.log('hello world');
+
+
+// chalk adalah package yg membuat output di console kita berwarna
+// menggunakan npm/package chalk
+console.log(chalk.blue.bgGreen('hello world'))
+
+// bisa mengguakan template literals
+const nama = 'mulya'
+const halo = `haloo, nama saya ${chalk.green(nama)}, saya berasal dari jurusan ${chalk.black.bgBlue('Teknik Infomratika')}`
+console.log(halo)
