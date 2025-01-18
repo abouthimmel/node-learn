9a7178e2c294344a65b840d656173790923d597a
abouthimmel <mpm111005@gmail.com>
2025-01-19 02:22:15 +0700

    initial commit

--- expressJS/app.js
+++ expressJS/app.js
@@ -28,6 +28,7 @@ app.get('/about', async(req, res) => {
   }
 })
 
+// mengirimkan dara berformat json
 app.get('/data-mahasiswa', async (req, res) => {
   try{
     const data = await fs.readFile('dataMahasiswa.json', 'utf8');
@@ -40,15 +41,39 @@ app.get('/data-mahasiswa', async (req, res) => {
   }
 })
 
+//atau jika ingin lebih mudah
+app.get('/tes', (req, res) => {
+  res.sendFile('tes.json', {root: __dirname});
+})
+
 // app.use('/', (req, res) => {
 //   res.status(404)
 //   res.send('<h1>404</h1> <br> Error: File not found');
   
 // })
 
-app.use((req, res) => {
-  res.status(404).send('<h1>404</h1><br>Error: File not found');
+app.get('/produk/:id/category/:idCat', (req, res) => {
+  res.send(`produkID :    <h1> ${req.params.id}</h1> \nCategory ID: ${req.params.idCat}`)
+}) // maka url localhost akan menjadi seperti ini localhost:300/produk(tergantung kita masukin berapa)/category(terserah masukin berapa), tapi ini nanti gabisa asal
+
+
+app.get('/produk/:id', (req, res) => {
+  res.send(`produkID : ${req.params.id} \ncategory : ${req.query.category}`)
+}) // maka url yg muncul adalah localhost:3000/produk(example:41)/?category=(example: shoes)
+
+
+
+app.use( async(req, res) => {
+  // res.status(404).send('<h1>404</h1> <h1>Error: File not found</h1>');
+  try{
+    const erpg = await fs.readFile('errorPage.html', 'utf-8');
+    res.send(erpg);
+  } catch(err) {
+    console.error(e);
+    res.send('Interval Server Error')
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ templating-engine/app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs/promises')
+
+
+// gunakan ejs
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/contact', async (req, res) => {
+  try{
+    const data = await fs.readFile('contact.html', 'utf8');
+    res.send(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Interval Server Error')
+  }
+})
+
+// membaca dari file index html lain
+app.get('/about', async(req, res) => {
+  try{
+    const data = await fs.readFile('about.html', 'utf8');
+    res.send(data)
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Interval Server Error')
+  }
+})
+
+// mengirimkan dara berformat json
+app.get('/data-mahasiswa', async (req, res) => {
+  try{
+    const data = await fs.readFile('dataMahasiswa.json', 'utf8');
+    // const jsonData = JSON.parse(data);
+    // res.send(data);
+    res.json(JSON.parse(data))
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Interval Server Error')
+  }
+})
+
+//atau jika ingin lebih mudah
+app.get('/tes', (req, res) => {
+  res.sendFile('tes.json', {root: __dirname});
+})
+
+app.get('/produk/:id/category/:idCat', (req, res) => {
+  res.send(`produkID :    <h1> ${req.params.id}</h1> \nCategory ID: ${req.params.idCat}`)
+}) // maka url localhost akan menjadi seperti ini localhost:300/produk(tergantung kita masukin berapa)/category(terserah masukin berapa), tapi ini nanti gabisa asal
+
+
+app.get('/produk/:id', (req, res) => {
+  res.send(`produkID : ${req.params.id} \ncategory : ${req.query.category}`)
+}) // maka url yg muncul adalah localhost:3000/produk(example:41)/?category=(example: shoes)
+
+
+
+app.use( async(req, res) => {
+  // res.status(404).send('<h1>404</h1> <h1>Error: File not found</h1>');
+  try{
+    const erpg = await fs.readFile('errorPage.html', 'utf-8');
+    res.send(erpg);
+  } catch(err) {
+    console.error(e);
+    res.send('Interval Server Error')
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
